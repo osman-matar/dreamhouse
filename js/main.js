5dfbe51d6a8e72980f47bea137880c2e936f3c5a
osman-matar <107319292+osman-matar@users.noreply.github.com>
2024-10-03 08:46:36 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,20 @@ const links = document.querySelector(".links");
 const link = document.querySelectorAll(".link");
 
 toggle.addEventListener("click", () => {
+    toggle.classList.toggle("orange"); 
     links.classList.toggle("active");
     link.forEach(link => {
         link.addEventListener("click",() => {
             links.classList.remove("active"); 
+            toggle.classList.remove("orange"); 
         })
     })
 })
 window.addEventListener("scroll", () => {
     links.classList.remove("active");
+    toggle.classList.remove("orange"); 
+    cartBody.classList.remove("show__cart"); 
+    cartBtn.classList.remove("orange") 
 });
 
 const cartBody = document.getElementById("cart");
@@ -21,6 +26,8 @@ const cartBtn = document.getElementById("cart__btn");
 cartBtn.addEventListener("click", () => {
     cartBody.classList.toggle("show__cart"); 
     cartBtn.classList.toggle("orange") 
+    links.classList.remove("active");
+    toggle.classList.remove("orange"); 
 })
 
 
